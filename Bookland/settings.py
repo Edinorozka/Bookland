13f167033a145c397ce5be1c0a123d66390ffa6d
Oleg Erofeev <125142060+Edinorozka@users.noreply.github.com>
2024-04-25 23:28:46 +0300

    Update settings.py

--- Bookland/settings.py
+++ Bookland/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR + "app", "static"),
+   os.path.join(BASE_DIR + "/app", "static"),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
